14c7be823c07359779fd90a393e1456dc0927952
kevinrojassanchez-lang <kevin.rojas.sanchez@est.una.ac.cr>
2026-03-25 16:23:10 -0600

    cambios en skill.h

--- Laboratorio4/Skill.h
+++ Laboratorio4/Skill.h
@@ -1,5 +1,26 @@
 #pragma once
+
 class Skill
 {
+ private:
+	std::string name;
+	std::string damageType;
+	int power;
+	int energyCost;
+
+	int clampRange(int value, int min, int max);
+	bool isValidType(std::string type);
+	
+
+public:
+	Skill(std::string name, std::string damageType, int power, int energyCost);
+	~Skill();
+	std::string getName() const;
+	std::string getDamageType() const;
+	int getPower() const;
+	int getEnergyCost() const;
+	int calculateEffect() const;
+	std::string toString() const;
 };
+}
 
